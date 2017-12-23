9ae11daceba98d4bfe4e920a4263c5b15e60f9ef
jstudenski <jstudenski@gmail.com>
2017-12-23 10:15:15 -0600

    added gen buttons function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,21 @@
 window.onload = function() {
 
 console.log('Hello World');
+var animals = ["Cat", "Dog", "Fish", "Flamingo"];
+
+
+function genBtns() { // clear div
+  $("#buttons-view").empty();
+  for (var i = 0; i < animals.length; i++) {
+    var btn = $("<button>");
+    btn.addClass("animal");
+    btn.attr("data-name", animals[i]);
+    btn.text(animals[i]);
+    $("#buttons-view").append(btn);
+  }
+}
+
+genBtns();
+
 
 } // window.onload
