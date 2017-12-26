33a68fff9185dabba631c0a24ef0d47a6cec8158
jstudenski <jstudenski@gmail.com>
2017-12-26 17:17:29 -0600

    started masonry style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,69 +5,65 @@ var animals = ["Cat", "Dog", "Fish", "Flamingo"];
 
 
 function genBtns() { // clear div
-  $("#buttons-view").empty();
+  $("#buttons").empty();
   for (var i = 0; i < animals.length; i++) {
     var btn = $("<button>");
     btn.addClass("animal");
     btn.attr("data-name", animals[i]);
     btn.text(animals[i]);
     btn.click(ajaxRequest);
-    $("#buttons-view").append(btn);
+    $("#buttons").append(btn);
   }
 }
 
 
 
 function ajaxRequest() {
+  $('.animal').removeClass("active");
+  $(this).addClass("active");
 
-console.log("HELLO");
 
-var animal = $(this).attr("data-name");
-var limit = 1;
-var apikey = 'dc6zaTOxFJmzC'
+  var animal = $(this).attr("data-name");
+  var limit = 12;
+  var apikey = 'dc6zaTOxFJmzC'
 
-var queryURL = "https://api.giphy.com/v1/gifs/search"
-  + "?q=" + animal 
-  + "&limit=" + limit
-  + "&api_key=" + apikey;
+  var queryURL = "https://api.giphy.com/v1/gifs/search"
+    + "?q=" + animal 
+    + "&limit=" + limit
+    + "&api_key=" + apikey;
 
 
 // Performing our AJAX GET request
 $.ajax({
     url: queryURL,
     method: "GET"
-  })
-  // After the data comes back from the API
-  .done(function(response) {
-    // Storing an array of results in the results variable
-    var results = response.data;
+  }).done(function(response) {
 
-    // Looping over every result item
+    var results = response.data;
+    $("#gifs-appear-here").empty();
+
     for (var i = 0; i < results.length; i++) {
 
       // Only taking action if the photo has an appropriate rating
       if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-        // Creating a div with the class "item"
-        var gifDiv = $("<div class='item'>");
 
-        // Storing the result item's rating
         var rating = results[i].rating;
+        var p = $("<p>").text(rating);
 
-        // Creating a paragraph tag with the result item's rating
-        var p = $("<p>").text("Rating: " + rating);
-
-
+        console.log(results[i])
 
         var animalGif = $("<img>");
         animalGif.attr({
           'data-state': 'still',
-          src: results[i].images.fixed_height_still.url,
-          'data-still': results[i].images.fixed_height_still.url,
-          'data-animate': results[i].images.fixed_height.url
+          src: results[i].images.fixed_width_still.url,
+          'data-still': results[i].images.fixed_width_still.url,
+          'data-animate': results[i].images.fixed_width.url
         }).click(animate);
 
-        gifDiv.append(p);
+        var gifDiv = $("<div class='item'>");
         gifDiv.append(animalGif);
+        gifDiv.append(p);
+
 
         // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
         $("#gifs-appear-here").prepend(gifDiv);
